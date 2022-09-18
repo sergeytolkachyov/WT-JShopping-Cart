68d775228a8b457062680317b7ac61a5a868a178
Sergey Tolkachyov <info@web-tolk.ru>
2022-09-18 15:13:43 +0400

    v.1.0.1

--- src/Helper/WtjshoppingcartHelper.php
+++ src/Helper/WtjshoppingcartHelper.php
@@ -1,10 +1,9 @@
 <?php
 /**
- * @package    WT JShopping Cart
+ * @package    WT JoomShopping cart
  * @author     Sergey Tolkachyov, info@web-tolk.ru https://web-tolk.ru
  * @copyright  Copyright (C) 2022 Sergey Tolkachyov. All rights reserved.
  * @license    GNU General Public License version 3 or later
- * @link 	   https://web-tolk.ru/en/dev/joomla-modules/wt-jshopping-cart-modul-bootstrap-5-korziny-dlya-joomshopping-5-i-joomla-4.html
  */
 
 namespace Joomla\Module\Wtjshoppingcart\Site\Helper;
@@ -25,6 +24,7 @@ class WtjshoppingcartHelper{
 
 		$cart = JSFactory::getModel('cart', 'jshop');
 		$cart->load("cart");
+		$cart->addLinkToProducts(1,$type="cart");
 		$ajax_cart = array(
 			'count_products' => $cart->count_product,
 			'price_product' => \JSHelper::formatprice($cart->price_product),
@@ -44,6 +44,7 @@ class WtjshoppingcartHelper{
 	{
 		$cart = JSFactory::getModel('cart', 'jshop');
 		$cart->load("cart");
+		$cart->addLinkToProducts(1,$type="cart");
 
 		return $cart;
 	}
